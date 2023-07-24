12590861c09a7d35aaa790b5d6eedbf66e3b87a0
Jonathan Bisson <bjonnh-git@bjonnh.net>
2023-07-23 22:34:30 -0500

    Fix container names

--- update_job.sh
+++ update_job.sh
@@ -10,7 +10,7 @@ TEXT_DATA_PATH=${TEXT_APP_PATH}/data
 
 cp ${MOL_DATA_PATH}/smiles.csv ${MOL_DATA_PATH}/smiles.csv.bak
 cp ${MOL_DATA_PATH}/structure_fps.pkl ${MOL_DATA_PATH}/structure_fps.pkl.bak
-docker run --user 0 -v ${MOL_APP_PATH}:/app -it mol_search bash -c "cd /app && python generate_database.py" && docker restart mol_search && echo "Successfuly updated mol_search" || (
+docker run --user 0 -v ${MOL_APP_PATH}:/app -it deployment-mol_search bash -c "cd /app && python generate_database.py" && docker restart mol_search && echo "Successfuly updated mol_search" || (
 cp ${MOL_DATA_PATH}/smiles.csv.bak ${MOL_DATA_PATH}/smiles.csv
 cp ${MOL_DATA_PATH}/structure_fps.pkl.bak ${MOL_DATA_PATH}/structure_fps.pkl
 )
@@ -19,7 +19,7 @@ cp ${MOL_DATA_PATH}/structure_fps.pkl.bak ${MOL_DATA_PATH}/structure_fps.pkl
 cp ${TEXT_DATA_PATH}/couples.csv ${TEXT_DATA_PATH}/couples.csv.bak
 cp ${TEXT_DATA_PATH}/taxa.csv ${TEXT_DATA_PATH}/taxa.csv.bak
 cp ${TEXT_DATA_PATH}/smiles.csv ${TEXT_DATA_PATH}/smiles.csv.bak
-docker run --user 0 -v ${TEXT_APP_PATH}:/app -it text_search bash -c "cd /app && python download_couples.py && python download_smiles.py && python download_taxa.py" && docker restart text_search && echo "Successfuly updated text_search" || (
+docker run --user 0 -v ${TEXT_APP_PATH}:/app -it deployment-text_search bash -c "cd /app && python download_couples.py && python download_smiles.py && python download_taxa.py" && docker restart text_search && echo "Successfuly updated text_search" || (
 cp ${TEXT_DATA_PATH}/couples.csv.bak ${TEXT_DATA_PATH}/couples.csv
 cp ${TEXT_DATA_PATH}/taxa.csv.bak ${TEXT_DATA_PATH}/taxa.csv
 cp ${TEXT_DATA_PATH}/smiles.csv.bak ${TEXT_DATA_PATH}/smiles.csv
