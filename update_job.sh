52f27b9ace01b25a0a38dbf7e6157bdfd94d4b58
Jonathan Bisson <bjonnh-git@bjonnh.net>
2023-07-23 20:54:24 -0500

    Update paths

--- update_job.sh
+++ update_job.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cp /home/bjo/molecule_search/app/data/smiles.csv /home/bjo/molecule_search/app/data/smiles.csv.bak
-cp /home/bjo/molecule_search/app/data/structure_fps.pkl /home/bjo/molecule_search/app/data/structure_fps.pkl.bak
-docker run --user 0 -v /home/bjo/molecule_search/app:/app -it mol_search bash -c "cd /app && python generate_database.py" && docker restart mol_search && echo "Successfuly updated" || (
-cp /home/bjo/molecule_search/app/data/smiles.csv.bak /home/bjo/molecule_search/app/data/smiles.csv
-cp /home/bjo/molecule_search/app/data/structure_fps.pkl.bak /home/bjo/molecule_search/app/data/structure_fps.pkl
+SOURCE_PATH=/home/bjo/deployment
+MOL_APP_PATH=${SOURCE_PATH}/mol_search
+MOL_DATA_PATH=${MOL_APP_PATH}/data
+
+cp ${MOL_DATA_PATH}/smiles.csv ${MOL_DATA_PATH}/smiles.csv.bak
+cp ${MOL_DATA_PATH}/structure_fps.pkl ${MOL_DATA_PATH}/structure_fps.pkl.bak
+docker run --user 0 -v ${MOL_APP_PATH}:/app -it mol_search bash -c "cd /app && python generate_database.py" && docker restart mol_search && echo "Successfuly updated" || (
+cp ${MOL_DATA_PATH}/smiles.csv.bak ${MOL_DATA_PATH}/smiles.csv
+cp ${MOL_DATA_PATH}/structure_fps.pkl.bak ${MOL_DATA_PATH}/structure_fps.pkl
 )
