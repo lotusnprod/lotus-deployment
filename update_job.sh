a7406d18b2d5304d4323f8eeef400b568a5877be
Jonathan Bisson <bjonnh-git@bjonnh.net>
2023-07-27 21:53:35 -0500

    Bla bla who cares about failures

--- update_job.sh
+++ update_job.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
 SOURCE_PATH=/home/bjo/deployment
-MOL_APP_PATH=${SOURCE_PATH}/mol_search
-MOL_DATA_PATH=${MOL_APP_PATH}/data
 
 TEXT_APP_PATH=${SOURCE_PATH}/text_search
 TEXT_DATA_PATH=${TEXT_APP_PATH}/data
-
-
-cp ${MOL_DATA_PATH}/smiles.csv ${MOL_DATA_PATH}/smiles.csv.bak
-cp ${MOL_DATA_PATH}/structure_fps.pkl ${MOL_DATA_PATH}/structure_fps.pkl.bak
-docker run --user 0 -v ${MOL_APP_PATH}:/app -it deployment-mol_search bash -c "cd /app && python generate_database.py" && docker restart mol_search && echo "Successfuly updated mol_search" || (
-cp ${MOL_DATA_PATH}/smiles.csv.bak ${MOL_DATA_PATH}/smiles.csv
-cp ${MOL_DATA_PATH}/structure_fps.pkl.bak ${MOL_DATA_PATH}/structure_fps.pkl
-)
-
 
 cp ${TEXT_DATA_PATH}/couples.csv ${TEXT_DATA_PATH}/couples.csv.bak
 cp ${TEXT_DATA_PATH}/taxa.csv ${TEXT_DATA_PATH}/taxa.csv.bak
