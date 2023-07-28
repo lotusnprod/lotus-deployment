7b41bc2b364530c24893a617cd8de898ddce7fc4
Jonathan Bisson <bjonnh-git@bjonnh.net>
2023-07-27 21:46:33 -0500

    Update the update job

--- update_job.sh
+++ update_job.sh
@@ -19,10 +19,10 @@ cp ${MOL_DATA_PATH}/structure_fps.pkl.bak ${MOL_DATA_PATH}/structure_fps.pkl
 cp ${TEXT_DATA_PATH}/couples.csv ${TEXT_DATA_PATH}/couples.csv.bak
 cp ${TEXT_DATA_PATH}/taxa.csv ${TEXT_DATA_PATH}/taxa.csv.bak
 cp ${TEXT_DATA_PATH}/smiles.csv ${TEXT_DATA_PATH}/smiles.csv.bak
-docker run --user 0 -v ${TEXT_APP_PATH}:/app -it deployment-text_search bash -c "cd /app && python download_couples.py && python download_smiles.py && python download_taxa.py" && docker restart text_search && echo "Successfuly updated text_search" || (
+cp ${TEXT_DATA_PATH}/structure_fps.pkl ${TEXT_DATA_PATH}/structure_fps.pkl.bak
+docker run --user 0 -v ${TEXT_APP_PATH}:/app -it deployment-text_search bash -c "cd /app && python download_couples.py && python download_smiles.py && python download_taxa.py && python generate_database.py" && docker restart text_search && echo "Successfuly updated text_search" || (
 cp ${TEXT_DATA_PATH}/couples.csv.bak ${TEXT_DATA_PATH}/couples.csv
 cp ${TEXT_DATA_PATH}/taxa.csv.bak ${TEXT_DATA_PATH}/taxa.csv
 cp ${TEXT_DATA_PATH}/smiles.csv.bak ${TEXT_DATA_PATH}/smiles.csv
+cp ${TEXT_DATA_PATH}/structure_fps.pkl.bak ${TEXT_DATA_PATH}/structure_fps.pkl
 )
-
-
