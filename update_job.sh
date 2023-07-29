dd878e73245940935c4475c6da3c8790886f303f
Jonathan Bisson <bjonnh-git@bjonnh.net>
2023-07-28 19:07:48 -0500

    Update all the stuff

--- update_job.sh
+++ update_job.sh
@@ -2,16 +2,16 @@
 
 SOURCE_PATH=/home/bjo/deployment
 
-TEXT_APP_PATH=${SOURCE_PATH}/text_search
-TEXT_DATA_PATH=${TEXT_APP_PATH}/data
+APP_PATH=${SOURCE_PATH}/search
+DATA_PATH=${APP_PATH}/data
 
-cp ${TEXT_DATA_PATH}/couples.csv ${TEXT_DATA_PATH}/couples.csv.bak
-cp ${TEXT_DATA_PATH}/taxa.csv ${TEXT_DATA_PATH}/taxa.csv.bak
-cp ${TEXT_DATA_PATH}/smiles.csv ${TEXT_DATA_PATH}/smiles.csv.bak
-cp ${TEXT_DATA_PATH}/structure_fps.pkl ${TEXT_DATA_PATH}/structure_fps.pkl.bak
-docker run --user 0 -v ${TEXT_APP_PATH}:/app -it deployment-text_search bash -c "cd /app && python download_couples.py && python download_smiles.py && python download_taxa.py && python generate_database.py" && docker restart text_search && echo "Successfuly updated text_search" || (
-cp ${TEXT_DATA_PATH}/couples.csv.bak ${TEXT_DATA_PATH}/couples.csv
-cp ${TEXT_DATA_PATH}/taxa.csv.bak ${TEXT_DATA_PATH}/taxa.csv
-cp ${TEXT_DATA_PATH}/smiles.csv.bak ${TEXT_DATA_PATH}/smiles.csv
-cp ${TEXT_DATA_PATH}/structure_fps.pkl.bak ${TEXT_DATA_PATH}/structure_fps.pkl
+cp ${DATA_PATH}/couples.csv ${DATA_PATH}/couples.csv.bak
+cp ${DATA_PATH}/taxa.csv ${DATA_PATH}/taxa.csv.bak
+cp ${DATA_PATH}/smiles.csv ${DATA_PATH}/smiles.csv.bak
+cp ${DATA_PATH}/structure_fps.pkl ${DATA_PATH}/structure_fps.pkl.bak
+docker run --user 0 -v ${APP_PATH}:/app -it deployment-search bash -c "cd /app && python update.py" && docker restart search && echo "Successfuly updated search" || (
+cp ${DATA_PATH}/couples.csv.bak ${DATA_PATH}/couples.csv
+cp ${DATA_PATH}/taxa.csv.bak ${DATA_PATH}/taxa.csv
+cp ${DATA_PATH}/smiles.csv.bak ${DATA_PATH}/smiles.csv
+cp ${DATA_PATH}/structure_fps.pkl.bak ${DATA_PATH}/structure_fps.pkl
 )
