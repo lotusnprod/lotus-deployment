1103d3004c0687724d4bf9091073d5130a19b09d
Jonathan Bisson <bjonnh-git@bjonnh.net>
2023-08-01 22:05:53 -0500

    Cleanup update script

--- update_job.sh
+++ update_job.sh
@@ -4,8 +4,9 @@ SOURCE_PATH=/home/bjo/deployment
 
 APP_PATH=${SOURCE_PATH}/search
 DATA_PATH=${APP_PATH}/data
-
+chown bjo:bjo ${DATA_PATH}/*
 cp ${DATA_PATH}/database.pkl ${DATA_PATH}/database.pkl.bak
 docker run --user 0 -v ${APP_PATH}:/app -it deployment-search bash -c "cd /app && python update.py" && docker restart search && echo "Successfuly updated search" || (
 cp ${DATA_PATH}/database.pkl.bak ${DATA_PATH}/database.pkl
+chown bjo:bjo ${DATA_PATH}/*
 )
