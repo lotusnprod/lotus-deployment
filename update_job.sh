b6c36c92500ace2ccfa5af7fd3a365011bf2f1e6
Jonathan Bisson <bjonnh-git@bjonnh.net>
2023-08-13 14:18:56 -0500

    Update scripts that were failing

--- update_job.sh
+++ update_job.sh
@@ -6,7 +6,7 @@ APP_PATH=${SOURCE_PATH}/search
 DATA_PATH=${APP_PATH}/data
 chown bjo:bjo ${DATA_PATH}/*
 cp ${DATA_PATH}/database.pkl ${DATA_PATH}/database.pkl.bak
-docker run --user 0 -v ${APP_PATH}:/app -it deployment-search bash -c "cd /app && python update.py" && docker restart search && echo "Successfuly updated search" || (
+docker run --user 0 -v ${APP_PATH}:/app -i deployment-search bash -c "cd /app && python update.py" && docker restart search && echo "Successfully updated search" || (
 cp ${DATA_PATH}/database.pkl.bak ${DATA_PATH}/database.pkl
 chown bjo:bjo ${DATA_PATH}/*
 )
